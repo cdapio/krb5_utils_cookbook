284164c915821f446ce187118a4745a2b3b22c41
Chris Gianelloni <chris@continuuity.com>
2014-05-01 17:19:10 -0700

    Adding files for chefspec testing

---
+++ spec/default_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'krb5_utils::default' do
+  context 'on Centos 6.4 x86_64' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(platform: 'centos', version: 6.4) do |node|
+        node.automatic['domain'] = 'example.com'
+        node.default['krb5_utils']['krb5_service_keytabs'] = {
+          'HTTP' => { 'owner' => 'hdfs', 'group' => 'hadoop', 'mode' => '0640' },
+          'hdfs' => { 'owner' => 'hdfs', 'group' => 'hadoop', 'mode' => '0640' }
+        }
+        node.default['krb5_utils']['krb5_user_keytabs'] = {
+          'yarn' => { 'owner' => 'yarn', 'group' => 'hadoop', 'mode' => '0640' }
+        }
+      end.converge(described_recipe)
+    end
+
+    it 'creates /etc/security/keytabs directory' do
+      expect(chef_run).to create_directory('/etc/security/keytabs')
+    end
+  end
+end
