7ad33ff11f10e93b9cd3880c392f004665aabdd7
Chris Gianelloni <chris@continuuity.com>
2014-08-04 17:13:37 -0700

    Use -norandkey on user keytab files

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chris@continuuity.com'
 license          'Apache License, Version 2.0'
 description      'Set of utility resources which can be used to setup Kerberos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'krb5'
 depends 'yum-epel'
