a69996bbf18ddea47182b0628e0dbd3c7c0ad0fa
Chris Gianelloni <chris@cask.co>
2014-09-25 15:01:48 -0700

    Constrain version of krb5 cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Set of utility resources which can be used to setup Kerberos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
-depends 'krb5'
+depends 'krb5', '~> 1.0'
 depends 'yum-epel'
